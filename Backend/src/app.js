1c75a8f990c0d307f32197089cb98029352796df
Jeevan Chavan <jeevanchavan7273@gmail.com>
2026-03-06 18:00:03 +0530

    render deployment changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from "url";
+
+// It creates __dirname manually in ES modules.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static("./public"));
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
@@ -16,5 +23,10 @@ import songRouter from './routes/song.routes.js'
 
 app.use('/api/auth',authRouter)
 app.use("/api/songs",songRouter)
+
+// wildcard api
+app.use("*name",(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","/public/index.html"));
+})
 
 export default app;
--- Frontend/src/features/home/services/song.api.js
+++ Frontend/src/features/home/services/song.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://vibe-sync-wpus.onrender.com",
     withCredentials:true
 })
 
